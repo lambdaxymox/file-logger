5c363519fc3049a9dd820df90f5bd19b9237950e
LambdaXymox <lambda.xymox@gmail.com>
2018-11-29 01:03:54 -0600

    Enable 'std' feature on log dependency so library will compile. Add some documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+extern crate chrono;
+extern crate log;
+
+mod file_logger;
+
+pub use file_logger::*;
